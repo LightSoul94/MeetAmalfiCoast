499f3bbd57c4246cd0860b9356ad9f1a2c167fb8
Gianluca Caliendo <gianlucacaliendo@libero.it>
2026-06-18 12:34:00 +0200

    .

--- wwwroot/js/firebaseConfig.js
+++ wwwroot/js/firebaseConfig.js
@@ -1,7 +1,11 @@
 const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  projectId: "test-909e7"
+    apiKey: "AIzaSyBHusQ0OEg2u52vebHV3PudaUV3zNtO87Q",
+    authDomain: "test-909e7.firebaseapp.com",
+    projectId: "test-909e7",
+    storageBucket: "test-909e7.firebasestorage.app",
+    messagingSenderId: "546623866516",
+    appId: "1:546623866516:web:6b96d7a06460dec28b5681",
+    measurementId: "G-V1YCNTFH1V"
 };
 
 firebase.initializeApp(firebaseConfig);
